223ebea11a1b2a3c6ee44498380e325f3a98f68d
jhj0224 <jhj8664@gmail.com>
2021-07-12 11:35:08 +0900

    about me 공간 작업중

--- src/view/usr/home/aboutMe.php
+++ src/view/usr/home/aboutMe.php
@@ -1,25 +1,30 @@
 <?php
-$pageTitleIcon = "";
-$pageTitle = "자기소개";
+$pageTitleIcon = '<i class="fas fa-user"></i>';
+$pageTitle = "내 정보";
 ?>
 
 <?php require_once __DIR__ . "/../head.php"; ?>
 
-<section class="section-1">
-  
-  <div class = "text-center">
-    <div>
+<section class="section-articles mt-4">
+  <div class="container mx-auto">
+  <div class="con-pad">
+  <div class="h-20 flex mt-4 sm:h-40 md:h-60 lg:h-80">
+  <div class="flex justify-center img-box w-2/3">     
     <br>
-    <img src="https://i.pinimg.com/564x/b3/f7/d0/b3f7d0db41a6016ccc29ceb2a19443a1.jpg" width="400" height="100" style = padding:30px>
-      <div class = "fas fa-user"> 정형준 Jeong Hyung Joon</div>
+    <img class="h-full" src="https://i.pinimg.com/564x/b3/f7/d0/b3f7d0db41a6016ccc29ceb2a19443a1.jpg" width="400" height="100" style = padding:30px>
+    <div class="flex flex-col px-5 justify-center justify-around flex-grow">  
+    <div class = "fas fa-user"> 정형준 Jeong Hyung Joon</div>
       <br>
       <span class = "fas fa-phone"> 010-6559-0224</span>
       <br>
       <li class="fas fa-envelope"> jhj8664@gmail.com</li>      
       <br>
       <br>
-      <div>간략한 자기소개 : 안녕하세요</div>
-      
+      <div>간략한 자기소개 : 안녕하세요. 신입 개발자 정형준입니다. 제 인생 첫 블로그 제작인만큼 열심히 만들어봤습니다. 많이 부족하겠지만 앞으로 더욱 더 실력을 갈고 닦아 좋은 개발자가 되도록 노력하겠습니다.</div>      
+      </div>
+</div>  
+    </div>
+      </div>
     </div>
   </div>
 </section>
